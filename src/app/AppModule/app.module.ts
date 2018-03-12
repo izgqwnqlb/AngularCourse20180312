511ce3fefe594c3035c2c9f56217540ee5dd1a6c
Matthias Hutsebaut <matthias.hutsebaut@gmail.com>
2018-03-12 13:24:24 +0100

    added first module

---
+++ src/app/AppModule/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
+@NgModule({
+    declarations:[],
+    providers:[],
+    imports:[BrowserModule],
+    exports:[],
+    bootstrap:[]
+})
+export class AppModule {
+
+}
+
